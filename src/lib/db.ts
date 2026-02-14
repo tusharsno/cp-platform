aa93348827233d143f9951dc96bde488875af647
Tushar Barua <tusharbarua269@gmail.com>
2026-02-14 13:36:22 +0600

    fix: prisma client import and location

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 // import { PrismaClient } from "../../generated/prisma";
-import { PrismaClient } from "@/generated/prisma";
+// import { PrismaClient } from "@/generated/prisma";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
