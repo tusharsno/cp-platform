999b8b8a15e9f427e9a875d99ba15a9851d19c21
Tushar Barua <tusharbarua269@gmail.com>
2026-02-14 14:16:55 +0600

    fix: explicit string casting for database url to resolve type error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,33 +1,11 @@
-// import { PrismaClient } from "@prisma/client";
-// // import { PrismaClient } from "../../generated/prisma";
-// // import { PrismaClient } from "@/generated/prisma";
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-
-// export const db = globalForPrisma.prisma ?? new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-
 import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "../../generated/prisma";
+// import { PrismaClient } from "@/generated/prisma";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+export const db = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
